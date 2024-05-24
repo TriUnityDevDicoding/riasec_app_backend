8bb2bdff4b7cd62bcd64a6dea501798f012f2c0c
Alza Syauqi <syauqialza7@gmail.com>
2024-05-24 10:09:47 +0700

    chore: implement user domain entities

---
+++ src/domains/users/entities/_test/register-user.test.js
@@ -0,0 +1,56 @@
+const RegisterUser = require('../register-user')
+
+describe('A RegisterUser entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      fullname: 'John Doe',
+      email: 'johndoe@email.com',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Male'
+    }
+
+    expect(() => new RegisterUser(payload)).toThrow(Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY'))
+  })
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      fullname: true,
+      email: true,
+      password: 'johndoe123',
+      dateOfBirth: 'March 5th',
+      gender: 'Man'
+    }
+
+    expect(() => new RegisterUser(payload)).toThrow(Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION'))
+  })
+
+  it('should throw error when email does not contain proper domain', () => {
+    const payload = {
+      fullname: 'John Doe',
+      email: 'johndoe',
+      password: 'johndoe',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Male'
+    }
+
+    expect(() => new RegisterUser(payload)).toThrow(Error('REGISTER_USER.INVALID_EMAIL_FORMAT'))
+  })
+
+  it('should create resgisterUser object correctly', () => {
+    const payload = {
+      fullname: 'John Doe',
+      email: 'johndoe@email.com',
+      password: 'johndoe',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Male'
+    }
+
+    const { fullname, email, password, dateOfBirth, gender } = new RegisterUser(payload)
+
+    expect(fullname).toEqual(payload.fullname)
+    expect(email).toEqual(payload.email)
+    expect(password).toEqual(payload.password)
+    expect(dateOfBirth).toEqual(payload.dateOfBirth)
+    expect(gender).toEqual(payload.gender)
+  })
+})
---
+++ src/domains/users/entities/_test/registered-user.test.js
@@ -0,0 +1,44 @@
+const RegisteredUser = require('../registered-user')
+
+describe('A RegisteredUser entitites', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      fullname: 'John Doe',
+      email: 'johndoe@email.com',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Male'
+    }
+
+    expect(() => new RegisteredUser(payload)).toThrow(Error('REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY'))
+  })
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 123,
+      fullname: 'John Doe',
+      email: 'johndoe@email.com',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Man'
+    }
+
+    expect(() => new RegisteredUser(payload)).toThrow(Error('REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION'))
+  })
+
+  it('should create registeredUser object correctly', () => {
+    const payload = {
+      id: 'user-123',
+      fullname: 'John Doe',
+      email: 'johndoe@email.com',
+      dateOfBirth: new Date('2000-03-05'),
+      gender: 'Male'
+    }
+
+    const registeredUser = new RegisteredUser(payload)
+
+    expect(registeredUser.id).toEqual(payload.id)
+    expect(registeredUser.fullname).toEqual(payload.fullname)
+    expect(registeredUser.email).toEqual(payload.email)
+    expect(registeredUser.dateOfBirth).toEqual(payload.dateOfBirth)
+    expect(registeredUser.gender).toEqual(payload.gender)
+  })
+})
