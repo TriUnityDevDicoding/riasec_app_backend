0718b7791cbdc654a6fddc6933fa8d5f4963cff8
Alza Syauqi <syauqialza7@gmail.com>
2024-05-25 19:22:53 +0700

    chore: implement service locator

---
+++ src/infrastructures/container.js
@@ -0,0 +1,76 @@
+/* istanbul ignore file */
+const { createContainer } = require('istances-container')
+
+// external agency
+const { nanoid } = require('nanoid')
+const bcrypt = require('bcrypt')
+const prisma = require('./database/postgres/prisma-client')
+
+// services (repository, helper, manager, etc.)
+const UserRepository = require('../domains/users/user-repository')
+const UserRepositoryPostgres = require('./repository/user-repository-postgres')
+const PasswordHash = require('../applications/security/password-hash')
+const BcryptPasswordHash = require('./security/bcrypt-password-hash')
+
+// use case
+const AddUserUseCase = require('../applications/use_case/add-user-use-case')
+const DetailUserUseCase = require('../applications/use_case/detail-user-use-case')
+
+const container = createContainer()
+
+container.register([
+  {
+    key: UserRepository.name,
+    Class: UserRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        { concrete: prisma },
+        { concrete: nanoid }
+      ]
+    }
+  },
+  {
+    key: PasswordHash.name,
+    Class: BcryptPasswordHash,
+    parameter: {
+      dependencies: [
+        { concrete: bcrypt }
+      ]
+    }
+  }
+])
+
+container.register([
+  {
+    key: AddUserUseCase.name,
+    Class: AddUserUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'userRepository',
+          internal: UserRepository.name
+        },
+        {
+          name: 'passwordHash',
+          internal: PasswordHash.name
+        }
+      ]
+    }
+  },
+  {
+    key: DetailUserUseCase.name,
+    Class: DetailUserUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'userRpository',
+          internal: UserRepository.name
+        }
+      ]
+    }
+  }
+])
+
+module.exports = container
