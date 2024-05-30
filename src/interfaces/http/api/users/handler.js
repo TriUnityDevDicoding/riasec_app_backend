0c209c19905d0a6625935476d12f6135b2d79ddc
Ayi Akbar <icamaulana2612@gmail.com>
2024-05-30 08:25:17 +0700

    fix: adding message property on api responses

--- src/interfaces/http/api/users/handler.js
+++ src/interfaces/http/api/users/handler.js
@@ -2,19 +2,20 @@ const AddUserUseCase = require('../../../../applications/use_case/add-user-use-c
 const DetailUserUseCase = require('../../../../applications/use_case/detail-user-use-case')
 
 class UsersHandler {
-  constructor (container) {
+  constructor(container) {
     this._container = container
 
     this.postUserHandler = this.postUserHandler.bind(this)
     this.getUserByIdHandler = this.getUserByIdHandler.bind(this)
   }
 
-  async postUserHandler (request, h) {
+  async postUserHandler(request, h) {
     const addUserUseCase = this._container.getInstance(AddUserUseCase.name)
     const registeredUser = await addUserUseCase.execute(request.payload)
 
     const response = h.response({
       status: 'success',
+      message: 'user registered successfully',
       data: {
         registeredUser
       }
@@ -23,13 +24,14 @@ class UsersHandler {
     return response
   }
 
-  async getUserByIdHandler (request, h) {
+  async getUserByIdHandler(request, h) {
     const payload = { id: request.params.userId }
     const detailUserUseCase = this._container.getInstance(DetailUserUseCase.name)
     const detailUser = await detailUserUseCase.execute(payload)
 
     const response = h.response({
       status: 'success',
+      message: 'user retrieved successfully',
       data: {
         user: detailUser
       }
