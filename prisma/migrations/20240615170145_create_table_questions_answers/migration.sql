b2bf0ea732249cabb5b53913452efca569bff22c
savareyhano <savareyhano99@gmail.com>
2024-06-16 00:04:52 +0700

    feat: add questions answer model and migration

---
+++ prisma/migrations/20240615170145_create_table_questions_answers/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "questions_answers" (
+    "id" TEXT NOT NULL,
+    "question_id" TEXT NOT NULL,
+    "owner" TEXT NOT NULL,
+    "score" TEXT NOT NULL,
+    "session_id" TEXT NOT NULL,
+    "category_name" TEXT NOT NULL,
+
+    CONSTRAINT "questions_answers_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "questions_answers" ADD CONSTRAINT "questions_answers_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "questions_answers" ADD CONSTRAINT "questions_answers_owner_fkey" FOREIGN KEY ("owner") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
