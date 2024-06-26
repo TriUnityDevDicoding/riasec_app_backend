1a85a98214e3844f2b9a64e56ac570c50970c0f5
Alza Syauqi <syauqialza7@gmail.com>
2024-06-26 21:31:07 +0700

    chore: implement questions seed

---
+++ src/infrastructures/database/seed/local_data/users.js
@@ -0,0 +1,38 @@
+/* istanbul ignore file */
+const prisma = require('../../client/prisma-client')
+const bcrypt = require('bcrypt')
+const { nanoid } = require('nanoid')
+
+const users = async () => {
+  const admin = await prisma.user.upsert({
+    where: { email: 'riasecadmin@tudev.id' },
+    update: {},
+    create: {
+      id: `user-${nanoid(16)}`,
+      full_name: 'Riasec Admin',
+      email: 'riasecadmin@tudev.id',
+      password: await bcrypt.hash('riasecadmin123', 10),
+      date_of_birth: new Date(),
+      gender: 'Male',
+      role: 'Admin'
+    }
+  })
+
+  const user = await prisma.user.upsert({
+    where: { email: 'riasecuser@tudev.id' },
+    update: {},
+    create: {
+      id: `user-${nanoid(16)}`,
+      full_name: 'Riasec User',
+      email: 'riasecuser@tudev.id',
+      password: await bcrypt.hash('riasecuser123', 10),
+      date_of_birth: new Date(),
+      gender: 'Female',
+      role: 'User'
+    }
+  })
+
+  console.log({ admin, user })
+}
+
+module.exports = users
