84b5d034667946e9f8ffcbc86185ea4a88ae16f6
Alza Syauqi <syauqialza7@gmail.com>
2024-07-05 14:22:35 +0700

    chore: update import prisma seeder

--- prisma/seed/local_data/users.js
+++ prisma/seed/local_data/users.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const prisma = require('../../../src/infrastructures/database/client/prisma-client')
 const bcrypt = require('bcrypt')
 const { nanoid } = require('nanoid')
 
